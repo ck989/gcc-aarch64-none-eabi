2278fb0ea6c35246eb09ea7e20d7c7984059fa45
ck989 <chayakumar33@gmail.com>
2025-07-21 00:38:07 -0700

    Added layout defination for conan package creator.

--- conanfile.py
+++ conanfile.py
@@ -5,11 +5,22 @@ from conan import ConanFile
 class GCCAArch64NoneEabiRecipe(ConanFile):
     name = "gcc-aarch64-none-eabi"
     version = "14.2.1"
+    package_type = "application"
+    settings = "os", "arch"
+    exports_sources = "gcc/*"
+
+    def layout(self):
+        self.folders.source = "."
+        self.folders.build = "build"
+        self.folders.generators = "build/generators"
+        self.cpp.package.resdirs = []
+        self.cpp.package.includedirs = []
+        self.cpp.package.libdirs = []
+        self.cpp.package.bindirs = ["bin"]
 
     def build(self):
         url = "https://github.com/ck989/gcc-aarch64-none-eabi.git"
         self.run(f"git clone --depth=1 {url} gcc")
-
         return
     
     def package(self):
@@ -17,6 +28,6 @@ class GCCAArch64NoneEabiRecipe(ConanFile):
         return
     
     def package_info(self):
-        self.buildenv_info.append_path("PATH",os.path.join(self.package_folder, "bin"))
+        self.buildenv_info.append_path("PATH", os.path.join(self.package_folder, "bin"))
         return
 
